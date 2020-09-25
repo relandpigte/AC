be75ebb116f19cd837d2e281900f7b0036f17fc5
Reland Pigte <reland102088@gmail.com>
2020-09-25 19:03:22 +0800

    (feature) [AC-97] : Proposals page further enhancements

---
+++ code/academically-admin/src/app/widgets/proposals-contacts-widgets/proposals-contacts-widgets.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'proposals-contacts-widgets',
+  templateUrl: './proposals-contacts-widgets.component.html',
+  styleUrls: ['./proposals-contacts-widgets.component.less']
+})
+export class ProposalsContactsWidgetsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
