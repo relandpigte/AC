1d32e0e1fe88836ac9f08f50a7a2d02481524f84
Reland Pigte <reland102088@gmail.com>
2020-11-06 11:55:37 +0800

    (hotfix) : set googleApiKey for staging

--- code/academically-admin/src/environments/environment.staging.ts
+++ code/academically-admin/src/environments/environment.staging.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   hmr: false,
-  appConfig: 'appconfig.staging.json'
+  appConfig: 'appconfig.staging.json',
+  googleApiKey: 'AIzaSyBtYaldOOmA-wNFOI7f32MvYSYGkaLAz6k',
 };
