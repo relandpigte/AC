7cb80c2c046defd45e9b11b2c0fef28640721d34
Reland Pigte <reland102088@gmail.com>
2020-11-16 18:25:38 +0800

    (feature) [AC-164]: Able to send offer to student

---
+++ code/Academically/Academically.Application/Services/Offers/Dto/CreateTutorOfferDto.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using Academically.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Academically.Services.Offers.Dto
+{
+    public class CreateTutorOfferDto
+    {
+        public Guid TutorialId { get; set; }
+        public long StudentId { get; set; }
+        public bool IsSubmitted { get; set; }
+    }
+}
---
+++ code/Academically/Academically.Core/Entities/TutorOffer.cs
@@ -0,0 +1,18 @@
+﻿using Abp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Academically.Entities
+{
+    [Table("AcademicallyTutorOffers")]
+    public class TutorOffer : Entity<Guid>
+    {
+        public Guid TutorialId { get; set; }
+        public long TutorId { get; set; }
+        public bool IsAccepted { get; set; }
+        public bool IsSubmitted { get; set; }
+        public DateTime CreationTime { get; set; }
+    }
+}
