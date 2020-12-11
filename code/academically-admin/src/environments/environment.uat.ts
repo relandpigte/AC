eac95ecac3fd9377075306ed0bdf5f71478db3e4
Reland Pigte <reland102088@gmail.com>
2020-12-11 12:15:08 +0800

    (hotfix) : added agora configurations for other environments

--- code/academically-admin/src/environments/environment.uat.ts
+++ code/academically-admin/src/environments/environment.uat.ts
@@ -3,4 +3,8 @@ export const environment = {
   hmr: false,
   appConfig: 'appconfig.uat.json',
   googleApiKey: 'AIzaSyBtYaldOOmA-wNFOI7f32MvYSYGkaLAz6k',
+  agora: {
+    appId: '61dfaf37a1ca4dfdb89d305d29914a09',
+    appCertificate: '37b27c2d394341b19886a401211c16d8',
+  },
 };
