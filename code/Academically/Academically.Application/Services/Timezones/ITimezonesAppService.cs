728fc28dbc2ff93743651a0f4f924a28465e7ead
Reland Pigte <reland102088@gmail.com>
2020-12-17 14:15:08 +0800

    (feature) [AC-232]: Added a page for guardian's approval

---
+++ code/Academically/Academically.Application/Services/Timezones/ITimezonesAppService.cs
@@ -0,0 +1,14 @@
+﻿using Abp.Application.Services;
+using Academically.Services.Timezones.Dto;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Academically.Services.Timezones
+{
+    public interface ITimezonesAppService : IApplicationService
+    {
+        IEnumerable<TimezoneInfoDto> GetTimezonesList();
+    }
+}
---
+++ code/Academically/Academically.Application/Services/Timezones/TimezonesAppService.cs
@@ -0,0 +1,25 @@
+﻿using Academically.Services.Timezones.Dto;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academically.Services.Timezones
+{
+    public class TimezonesAppService : AcademicallyAppServiceBase, ITimezonesAppService
+    {
+        public TimezonesAppService()
+        {
+
+        }
+
+        public IEnumerable<TimezoneInfoDto> GetTimezonesList()
+        {
+            var timezones = TimeZoneInfo.GetSystemTimeZones();
+            var result = timezones.Select(t => ObjectMapper.Map<TimezoneInfoDto>(t));
+            return result;
+        }
+    }
+}
