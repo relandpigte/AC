2042272c4230af7bdfb683c61a2797c0fa54792d
Reland Pigte <reland102088@gmail.com>
2021-06-23 14:48:12 +0800

    (hotfix) : wireup api endpoint to recent projects widget in dashboard.

--- code/new/academically-admin/src/app/widgets/recent-projects/recent-projects.component.ts
+++ code/new/academically-admin/src/app/widgets/recent-projects/recent-projects.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppComponentBase } from '@shared/app-component-base';
+import { ProjectDto, ProjectDtoPagedResultDto, ProjectsServiceProxy } from '@shared/service-proxies/service-proxies';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-recent-projects',
   templateUrl: './recent-projects.component.html',
   styleUrls: ['./recent-projects.component.less']
 })
-export class RecentProjectsComponent implements OnInit {
+export class RecentProjectsComponent extends AppComponentBase implements OnInit {
+  isLoading = false;
+  projects: ProjectDto[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    injector: Injector,
+    private _router: Router,
+    private _projectsService: ProjectsServiceProxy
+  ) {
+    super(injector);
   }
 
+  ngOnInit(): void {
+    this.getProjects();
+  }
+
+  onViewClick(project: ProjectDto): void {
+    this._router.navigate(['/app/projects/' + project.id + '/proposals']);
+  }
+
+  private getProjects(): void {
+    this._projectsService
+      .getAll(this.appSession.userId, '', 1, 5)
+      .pipe( finalize(() =>  this.isLoading = false))
+      .subscribe((result: ProjectDtoPagedResultDto) => {
+        this.projects = result.items;
+      });
+  }
 }
