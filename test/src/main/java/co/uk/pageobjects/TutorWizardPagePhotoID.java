d9b41fd778476f75c38b44950fec54e6001afe61
Reland Pigte <reland102088@gmail.com>
2021-06-28 16:12:39 +0800

    AC-413 #comment added tutor wizard page object

---
+++ test/src/main/java/co/uk/pageobjects/TutorWizardPagePhotoID.java
@@ -0,0 +1,49 @@
+package co.uk.pageobjects;
+
+import org.openqa.selenium.By;
+
+import co.uk.core.DriverHandler;
+import co.uk.webelements.Button;
+import co.uk.webelements.Element;
+import co.uk.webelements.TextBox;
+
+public class TutorWizardPagePhotoID {
+
+	private static TextBox documentUploader = new TextBox("Photo file",By.xpath("//app-photo-id//input[@id='DocumentUploader']"));
+	private static Button next = new Button("Next",By.xpath("//button[contains(text(),'Next')]"));
+	
+	public static void uploadProfilePhoto(String filePath) {
+		documentUploader.uploadFile(filePath);
+	    DriverHandler.delay(5);
+	}
+	
+	public static void clickNext() {
+		next.click();
+	}
+	  
+	public static class CropImageModal {
+
+        private static Element cropImageModal = new Element("Crop image",
+                By.xpath("//h4[@class='card-header-title' and contains(text(),'Crop Image')]"));
+        private static Button crop = new Button("Crop", By.xpath("//span[contains(text(),'Crop')]/parent::button"));
+        private static Button cancel = new Button("Cancel", By.xpath("//Button[contains(text(),'Cancel')]"));
+        private static Element successfulMessage = new Element("Your profile picture was uploaded",
+                By.xpath("//span[text()='Your profile picture was uploaded.']"));
+
+        public static void verifySuccessfulMessageIsDisplayed() {
+            successfulMessage.verifyDisplayed();
+        }
+
+        public static void verifyCropImageModalIsDisplayed() {
+            cropImageModal.verifyDisplayed();
+        }
+
+        public static void clickCrop() {
+            crop.click();
+        }
+
+        public static void clickCancel() {
+            cancel.click();
+        }
+    }
+}
