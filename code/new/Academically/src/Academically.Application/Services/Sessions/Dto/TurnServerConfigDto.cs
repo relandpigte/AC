4973fa264715388f0616606eafc18c39f199261d
Reland Pigte <reland102088@gmail.com>
2021-08-09 08:29:29 +0000

    (feature) [AC-Sessions] : added configuration of turn server credentials

---
+++ code/new/Academically/src/Academically.Application/Services/Sessions/Dto/TurnServerConfigDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Academically.Services.Sessions.Dto
+{
+    public class TurnServerConfigDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
