162da96ff5248bb7eb94681b7540f28cd244c763
Reland Pigte <reland102088@gmail.com>
2021-09-23 12:37:25 +0800

    (feature) [AC-795] : added option for user to select blank template when creating a new lesson

--- code/new/academically-admin/src/app/curriculum/curriculum.module.ts
+++ code/new/academically-admin/src/app/curriculum/curriculum.module.ts
@@ -5,10 +5,12 @@ import { CurriculumRoutingModule } from './curriculum-routing.module';
 import { CurriculumComponent } from './curriculum.component';
 import { SharedModule } from '@shared/shared.module';
 import { AppSharedModule } from '@app/_shared/app-shared.module';
+import { LessonWizardComponent } from './_components/lesson-wizard/lesson-wizard.component';
 
 @NgModule({
   declarations: [
     CurriculumComponent,
+    LessonWizardComponent,
   ],
   imports: [
     CommonModule,
