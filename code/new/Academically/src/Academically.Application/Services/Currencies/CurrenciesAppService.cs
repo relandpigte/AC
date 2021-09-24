111afa5d13a40bc9f517cf45610906e557c18228
Reland Pigte <reland102088@gmail.com>
2021-09-24 14:28:26 +0800

    (feature) [LMS] : added saving of pricing

---
+++ code/new/Academically/src/Academically.Application/Services/Currencies/CurrenciesAppService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Abp.Domain.Repositories;
+using Academically.Domain.Entities;
+using Academically.Services.Currencies.Dto;
+using Microsoft.EntityFrameworkCore;
+
+namespace Academically.Services.Currencies
+{
+    public class CurrenciesAppService : AcademicallyAppServiceBase, ICurrenciesAppService
+    {
+        private readonly IRepository<Currency, Guid> _currenciesRepository;
+
+        public CurrenciesAppService(
+            IRepository<Currency, Guid> currenciesRepository
+            )
+        {
+            _currenciesRepository = currenciesRepository;
+        }
+
+        public async Task<IEnumerable<CurrencyDto>> GetAll()
+        {
+            return await _currenciesRepository.GetAll()
+                .OrderBy(e => e.Code)
+                .Select(e => ObjectMapper.Map<CurrencyDto>(e))
+                .ToListAsync();
+        }
+    }
+}
---
+++ code/new/Academically/src/Academically.Application/Services/Currencies/ICurrenciesAppService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Abp.Application.Services;
+using Academically.Services.Currencies.Dto;
+
+namespace Academically.Services.Currencies
+{
+    public interface ICurrenciesAppService : IApplicationService
+    {
+        Task<IEnumerable<CurrencyDto>> GetAll();
+    }
+}
