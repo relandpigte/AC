3587c89628dab1f5c4057989937c3ac1e3e0604c
Reland Pigte <reland102088@gmail.com>
2021-09-29 12:52:59 +0800

    (feature) [LMS] : added schema and entity for CourseSection

---
+++ code/new/Academically/src/Academically.Core/Domain/Entities/CourseSection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Abp.Domain.Entities.Auditing;
+using Academically.Domain.Enums;
+
+namespace Academically.Domain.Entities
+{
+    [Table("AcademicallyCourseSections")]
+    public class CourseSection : CreationAuditedEntity<Guid>
+    {
+        public string Name { get; set; }
+        public CourseSectionType Type { get; set; }
+        public CourseSectionStatus Status { get; set; }
+        public Guid CourseId { get; set; }
+        public Guid? ParentId { get; set; }
+
+        [ForeignKey("CourseId")]
+        public virtual Course Course { get; set; }
+        [ForeignKey("ParentId")]
+        public virtual CourseSection Parent { get; set; }
+    }
+}
