bf21142aa48b8cfe0db1e02b5c8cc0a9a7ddeb1c
Reland Pigte <reland102088@gmail.com>
2021-11-25 11:14:30 +0800

    (hotfix) [tutor-portal] : fixed issue on courses tab for dashboard where links are not working

--- code/new/academically-admin/src/app/dashboard/courses/courses.module.ts
+++ code/new/academically-admin/src/app/dashboard/courses/courses.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { SharedModule } from '@shared/shared.module';
 import { AppSharedModule } from '@app/_shared/app-shared.module';
 
@@ -17,6 +18,7 @@ import { CourseNameComponent } from './course-name/course-name.component';
   ],
   imports: [
     CommonModule,
+    RouterModule,
     SharedModule,
     AppSharedModule,
   ],
