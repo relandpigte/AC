472c2561479f4a46b3b9bcf9603f685f0cf1dd02
Reland Pigte <reland102088@gmail.com>
2022-10-02 06:11:50 +0000

    AC-1331 top 3 new coaching in for you page

--- code/new/academically-admin/src/app/explore/for-you/for-you.component.ts
+++ code/new/academically-admin/src/app/explore/for-you/for-you.component.ts
@@ -1,12 +1,11 @@
 import { Component, Injector, OnInit } from '@angular/core';
 import { appModuleAnimation } from '@shared/animations/routerTransition';
 import { AppComponentBase } from '@shared/app-component-base';
-import { VideoDto, VideosServiceProxy, DateGrains, VideoDtoPagedResultDto, EventDto, EventsServiceProxy } from '@shared/service-proxies/service-proxies';
+import { VideoDto, VideosServiceProxy, DateGrains, VideoDtoPagedResultDto, EventDto, EventsServiceProxy, CoachingsServiceProxy, CoachingDto } from '@shared/service-proxies/service-proxies';
 import { finalize, takeUntil } from 'rxjs/operators';
 
 import * as moment from 'moment';
 import * as _ from 'lodash';
-import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 
 @Component({
   selector: 'app-explore-for-you',
@@ -18,16 +17,19 @@ export class ExploreForYouComponent extends AppComponentBase implements OnInit {
 
   top3Tutorials: VideoDto[] = Array(3).fill([]).map(() => this.generateRandomTutorial()) as VideoDto[];
   top3UpcomingEvent: EventDto[] = Array(3).fill([]).map(() => this.generateRandomEvent()) as EventDto[];
+  top3coaching: CoachingDto[] = Array(3).fill([]).map(() => this.generateRandomCoaching()) as CoachingDto[];
 
   top3VideosData: VideoDtoPagedResultDto;
 
   isLoadingTutorials = true;
   isLoadingEvents = true;
+  isLoadingCoaching = true;
 
   constructor(
     injector: Injector,
     private _videoService: VideosServiceProxy,
     private _eventsService: EventsServiceProxy,
+    private _coachingsService: CoachingsServiceProxy
   ) {
     super(injector);
   }
@@ -35,6 +37,7 @@ export class ExploreForYouComponent extends AppComponentBase implements OnInit {
   ngOnInit(): void {
     this.loadDataTutorials();    
     this.loadDataEvents();
+    this.loadDataCoaching();
   }
 
   private loadDataTutorials(): void {
@@ -52,6 +55,18 @@ export class ExploreForYouComponent extends AppComponentBase implements OnInit {
         } 
     });
   }
+  private loadDataCoaching(): void {
+    this.isLoadingCoaching = true;
+    this._coachingsService.getByDates(DateGrains.Monthly, 3)
+      .pipe(takeUntil(this.destroyed$))
+      .pipe(finalize(() => this.isLoadingCoaching = false))
+      .subscribe(coachings => {
+        this.top3coaching = [];
+        Object.keys(coachings).forEach(range => {
+            this.top3coaching = _.concat(this.top3coaching, coachings[range]);
+        });
+    });
+  }
 
   private loadDataEvents(): void {
     this.isLoadingEvents = true;
@@ -65,7 +80,7 @@ export class ExploreForYouComponent extends AppComponentBase implements OnInit {
             this.top3UpcomingEvent = _.concat(this.top3UpcomingEvent, events[range]);
           });
         }        
-    });
+    });       
   }
   
 }
