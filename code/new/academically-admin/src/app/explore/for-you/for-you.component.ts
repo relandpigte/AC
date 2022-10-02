111e2521f2869b8d5bda91620f708a3f7e6f2fd7
Reland Pigte <reland102088@gmail.com>
2022-10-02 05:58:59 +0000

    AC-1330 - i want to see recommended tutorials

--- code/new/academically-admin/src/app/explore/for-you/for-you.component.ts
+++ code/new/academically-admin/src/app/explore/for-you/for-you.component.ts
@@ -1,11 +1,12 @@
 import { Component, Injector, OnInit } from '@angular/core';
 import { appModuleAnimation } from '@shared/animations/routerTransition';
 import { AppComponentBase } from '@shared/app-component-base';
-import { DateGrains, EventDto, EventsServiceProxy } from '@shared/service-proxies/service-proxies';
+import { VideoDto, VideosServiceProxy, DateGrains, VideoDtoPagedResultDto, EventDto, EventsServiceProxy } from '@shared/service-proxies/service-proxies';
 import { finalize, takeUntil } from 'rxjs/operators';
 
 import * as moment from 'moment';
 import * as _ from 'lodash';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 
 @Component({
   selector: 'app-explore-for-you',
@@ -15,52 +16,56 @@ import * as _ from 'lodash';
 })
 export class ExploreForYouComponent extends AppComponentBase implements OnInit {
 
+  top3Tutorials: VideoDto[] = Array(3).fill([]).map(() => this.generateRandomTutorial()) as VideoDto[];
   top3UpcomingEvent: EventDto[] = Array(3).fill([]).map(() => this.generateRandomEvent()) as EventDto[];
 
-  isLoading = true;
+  top3VideosData: VideoDtoPagedResultDto;
+
+  isLoadingTutorials = true;
+  isLoadingEvents = true;
 
   constructor(
     injector: Injector,
+    private _videoService: VideosServiceProxy,
     private _eventsService: EventsServiceProxy,
   ) {
     super(injector);
   }
 
   ngOnInit(): void {
+    this.loadDataTutorials();    
     this.loadDataEvents();
+  }
 
-    //this.loadDataCoaching();
+  private loadDataTutorials(): void {
+    this.isLoadingTutorials = true;
+    this._videoService.getByDates(this.appSession.userId, undefined, undefined, undefined, DateGrains.Monthly, undefined, 3)
+      .pipe(takeUntil(this.destroyed$))
+      .pipe(finalize(() => this.isLoadingTutorials = false))
+      .subscribe(videos => {       
+        if (videos) {          
+          this.top3Tutorials = [];
+
+          Object.keys(videos).forEach(range => {
+            this.top3VideosData = videos[range];              
+          });
+        } 
+    });
   }
 
   private loadDataEvents(): void {
-    this.isLoading = true;
-    this._eventsService.getByDates(DateGrains.Monthly, 6)
+    this.isLoadingEvents = true;
+    this._eventsService.getByDates(DateGrains.Monthly, 3)
       .pipe(takeUntil(this.destroyed$))
-      .pipe(finalize(() => this.isLoading = false))
+      .pipe(finalize(() => this.isLoadingEvents = false))
       .subscribe(events => {
         if (events) {
           this.top3UpcomingEvent = [];
           Object.keys(events).forEach(range => {
-          const [startDate] = range.split(' - ');
-          if (!(moment().diff(moment(startDate), 'months')))
-            this.top3UpcomingEvent = _.concat(this.top3UpcomingEvent, events[range]);          
-          
+            this.top3UpcomingEvent = _.concat(this.top3UpcomingEvent, events[range]);
           });
         }        
     });
   }
-
-  // private generateData(count?: number, type?: number): any[] {
-  //   let data = Array(count).fill([]).map(() => {
-  //     const dataType = this.randomNonZero(6);
-  //     switch(type ?? dataType) {
-  //       case 1: return this.generateRandomEvent();
-  //       case 2: return this.generateRandomArticle();
-  //       case 3: return this.generateRandomCoaching();
-  //       case 4: return this.generateRandomCourse();
-  //       case 5: return this.generateRandomWorkshop();
-  //     }
-  //   });
-  //   return data;
-  // }
+  
 }
