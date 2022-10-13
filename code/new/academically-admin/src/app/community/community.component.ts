5c0a23f05a980995aa8a2979ddfabacb59f14e4e
Reland Pigte <reland102088@gmail.com>
2022-10-13 20:06:47 +0800

    AC-1374 Updates

--- code/new/academically-admin/src/app/community/community.component.ts
+++ code/new/academically-admin/src/app/community/community.component.ts
@@ -1,7 +1,7 @@
 import { Component, Injector, OnInit } from '@angular/core';
 import { appModuleAnimation } from '@shared/animations/routerTransition';
 import { AppComponentBase } from '@shared/app-component-base';
-import { CourseDto, CoursesServiceProxy, DateGrains } from '@shared/service-proxies/service-proxies';
+import { CourseDto, CoursesServiceProxy, DateGrains, UserDto, UserServiceProxy } from '@shared/service-proxies/service-proxies';
 import { takeUntil } from 'rxjs/operators';
 
 import * as _ from 'lodash';
@@ -16,12 +16,14 @@ export class CommunityComponent extends AppComponentBase implements OnInit {
 
   selectedTopics: string[] = [];
   suggestedTopics: { topic: string, followers: number }[] = [];
+  peopleToFollow: UserDto[] = [];
   recommendedCourses: CourseDto[] = [];
 
   get topics(): string[] { return ['Test', 'Sample 10122022', 'Astronomy', 'Biology', 'Fiction']; }
 
   constructor(
     injector: Injector,
+    private _usersService: UserServiceProxy,
     private _coursesService: CoursesServiceProxy
   ) {
     super(injector);
@@ -29,10 +31,23 @@ export class CommunityComponent extends AppComponentBase implements OnInit {
 
   ngOnInit(): void {
     this.getSuggestedTopics();
+    this.getPeopleToFollow();
+    this.getRecommendedCourses();
   }
 
   handleFilterTopics(topics: string[]): void {
     this.selectedTopics = topics;
+  }
+
+  getCourseThumbnail(course: CourseDto): string {
+    return course.thumbnailImageUrl ?? 'assets/img/img-placeholder.png';
+  }
+
+  getCourseComposition(course: CourseDto): string {
+    const modules = course?.modules ? `${course?.modules} modules` : null;
+    const lessons = course?.lessons ? `${course?.lessons} lessons` : null;
+    const values = [modules, lessons].filter(x => x);
+    return values.join(', ');
   }
 
   getSuggestedTopics(): void {
@@ -42,6 +57,12 @@ export class CommunityComponent extends AppComponentBase implements OnInit {
       { topic: "OkiDokiCollective", followers: 1220 },
       { topic: "Trending", followers: 4220 }
     ];
+  }
+
+  getPeopleToFollow(): void {
+    this._usersService.getAll('', true, 'creationTime desc', 0, 4)
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(pagedUsers => this.peopleToFollow = pagedUsers.items ?? [])
   }
 
   getRecommendedCourses(): void {
@@ -55,8 +76,7 @@ export class CommunityComponent extends AppComponentBase implements OnInit {
             this.recommendedCourses = _.concat(this.recommendedCourses, courses[range]?.items);
           });
         }
-
-      })
+      });
   }
 
 }
