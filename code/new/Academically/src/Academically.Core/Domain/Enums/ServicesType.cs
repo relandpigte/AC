59c021474db69868372db6350fcde6a96a6e0259
Reland Pigte <reland102088@gmail.com>
2022-12-08 01:42:52 +0800

    combined servicedto model

---
+++ code/new/Academically/src/Academically.Core/Domain/Enums/ServicesType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Academically.Domain.Enums
+{
+    public enum ServicesType
+    {
+        Event = 1,
+        Course = 2,
+        Video = 3,
+        Articles = 4,
+        Coaching = 5,
+        Workshop = 6,
+
+
+    }
+}
