82201be9227fb77a96a35f0fd1216e8761890201
Reland Pigte <reland102088@gmail.com>
2023-01-04 00:07:26 +0800

    [Update]: added post notification table script and entity

---
+++ code/new/Academically/src/Academically.Core/Domain/Entities/PostNotification.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Abp.Domain.Entities.Auditing;
+using Academically.Authorization.Users;
+
+namespace Academically.Domain.Entities
+{
+    [Table("AcademicallyPostNotification")]
+	public class PostNotification : CreationAuditedEntity<Guid>
+	{
+		public Guid PostId { get; set; }
+		public long? NotifyUserId { get; set; }
+
+		[ForeignKey("PostId")]
+		public virtual Post Post { get; set; }
+
+		[ForeignKey("NotifyUserId")]
+		public virtual User User { get; set; }
+	}
+}
+
