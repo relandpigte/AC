1ebb61365354294107b9f33dc1368d73c2def0c4
Reland Pigte <reland102088@gmail.com>
2023-07-27 07:59:26 +0000

    AC-61 Content cleaner function

---
+++ code/new/Academically/src/Academically.Functions.Content.Cleaner/ConsoleModule.cs
@@ -0,0 +1,46 @@
+﻿using Abp.Configuration.Startup;
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+using Academically.Configuration;
+using Academically.EntityFrameworkCore;
+using Academically.Functions.Content.Cleaner.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Academically.Functions.Content.Cleaner
+{
+    [DependsOn(typeof(AcademicallyEntityFrameworkModule))]
+    public class ConsoleModule : AbpModule
+    {
+        private readonly IConfigurationRoot _appConfiguration;
+
+        public ConsoleModule(AcademicallyEntityFrameworkModule abpProjectNameEntityFrameworkModule)
+        {
+            abpProjectNameEntityFrameworkModule.SkipDbSeed = true;
+
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            _appConfiguration = new ConfigurationBuilder()
+                .SetBasePath(typeof(ConsoleModule).GetAssembly().GetDirectoryPathOrNull())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true)
+                .Build();
+        }
+
+        public override void PreInitialize()
+        {
+            Configuration.DefaultNameOrConnectionString = 
+                _appConfiguration.GetConnectionString(AcademicallyConsts.ConnectionStringName);
+
+            Configuration.BackgroundJobs.IsJobExecutionEnabled = false;
+
+            Configuration.ReplaceService<IAppConfigurationAccessor, ConsoleConfigurationAccessor>();
+            Configuration.ReplaceService<IConfiguration, ConfigurationRoot>();
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(typeof(ConsoleModule).GetAssembly());
+            ServiceCollectionRegistrar.Register(IocManager);
+        }
+    }
+}
---
+++ code/new/Academically/src/Academically.Functions.Content.Cleaner/Function.cs
@@ -0,0 +1,52 @@
+﻿using Abp;
+using Abp.Castle.Logging.Log4Net;
+using Abp.Dependency;
+using Abp.Timing;
+using Amazon.Lambda.Core;
+using Castle.Facilities.Logging;
+using System;
+
+namespace Academically.Functions.Content.Cleaner
+{
+    public class Function
+    {
+        public void Handler(ILambdaContext context)
+        {
+            Console.WriteLine("Entry point to Content Cleaner function.");
+
+            using var bootstrapper = AbpBootstrapper.Create<ConsoleModule>();
+            try
+            {
+                Console.WriteLine("Initializing logging facility.");
+
+                bootstrapper.IocManager.IocContainer
+                .AddFacility<LoggingFacility>(f =>
+                    f.UseAbpLog4Net().WithConfig("log4net.config")
+                );
+                bootstrapper.Initialize();
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message == "Facility of type 'Castle.Facilities.Logging.LoggingFacility' has already been registered with the container. Only one facility of a given type can exist in the container.")
+                    Console.WriteLine("Suppress issue with re-registering LoggingFacility facility.");
+                else
+                    throw;
+            }
+
+            Console.WriteLine("Start resolving dependencies for Executer");
+
+            // Getting a Executer object from DI and running it
+            using (var executer = bootstrapper.IocManager.ResolveAsDisposable<Executer>())
+            {
+                Console.WriteLine($"Running Content.Cleaner executer. Date {Clock.Now:O}");
+                Console.WriteLine("------------------------------------------------------");
+
+                _ = executer.Object.RunAsync().Result;
+
+            } //Disposes executer and all it's dependencies
+
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine($"Done running Content.Cleaner executer. Date {Clock.Now:O}");
+        }
+    }
+}
